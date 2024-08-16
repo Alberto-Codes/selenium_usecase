f719f99de9073b6ab1a7c413763f14381663b85a
Alberto Nieto <94092485+Alberto-Codes@users.noreply.github.com>
2024-08-16 20:44:07 +0000

    refactor: clean up `db_pipeline.py` by removing redundant docstrings and simplifying code
    - Removed unnecessary docstrings that added little value or repeated information from the function names.
    - Simplified the task execution flow by directly using the `check_db_exists` result.

--- src/prefect_flows/db_pipeline.py
+++ src/prefect_flows/db_pipeline.py
@@ -1,23 +1,14 @@
 import os
 import sys
 
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "../../")))
 from prefect import flow, task
 
 from src.db.db_setup import create_database, destroy_database
 
-# Ensure the src directory is in the Python path
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "../../")))
-
 
 @task
 def create_db():
-    """
-    Task to create the database.
-
-    Returns:
-        sqlalchemy.engine.base.Engine: The SQLAlchemy engine connected to the
-        database.
-    """
     print("Creating database...")
     engine = create_database(echo=True)
     return engine
@@ -25,9 +16,6 @@ def create_db():
 
 @task
 def destroy_db():
-    """
-    Task to destroy the database by deleting the database file.
-    """
     print("Destroying database...")
     db_path = "data/rcn.db"
     destroy_database(db_path)
@@ -35,29 +23,16 @@ def destroy_db():
 
 @task
 def check_db_exists():
-    """
-    Task to check if the database file exists.
-
-    Returns:
-        bool: True if the database file exists, False otherwise.
-    """
     db_path = "data/rcn.db"
     return os.path.exists(db_path)
 
 
 @flow
 def db_management_flow(destroy=False):
-    """
-    Flow to manage database creation and destruction.
-
-    Args:
-        destroy (bool): If True, the database will be destroyed before
-            attempting to create it. Defaults to False.
-    """
     if destroy:
         destroy_db()
 
-    db_exists = check_db_exists().result()
+    db_exists = check_db_exists()
 
     if not db_exists:
         engine = create_db()
